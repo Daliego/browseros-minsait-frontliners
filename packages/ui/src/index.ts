faafe742a4c0e53b6b59355db1e69e2edfac7234
Daliego <diesgo.araujo@gmail.com>
2026-06-09 17:26:30 -0300

    fix(ui): replace shell scaffold page, fix Toaster name collision, fix contracts tsconfig for check-types

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -51,7 +51,7 @@ export * from "./components/table";
 export * from "./components/tabs";
 export * from "./components/textarea";
 export * from "./components/toast";
-export * from "./components/toaster";
+export { Toaster as RadixToaster } from "./components/toaster";
 export * from "./components/toggle";
 export * from "./components/toggle-group";
 export * from "./components/tooltip";
